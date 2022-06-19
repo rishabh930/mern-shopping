b139c7e1fa0e51e3cda9092a7d192547cdfa957b
rishabh930 <rishabhsharma467@gmail.com>
2022-06-19 10:21:25 +0530

    MERNAPP!

---
+++ mern/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="#">shoopingapp</a>
+      </header>
+      <main>list</main>
+    </div>
+  );
+};
+
+export default App;
