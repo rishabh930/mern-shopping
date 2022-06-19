0c62c8bf11f2f5d541477f8a34b652950f57d2f3
rishabh930 <rishabhsharma467@gmail.com>
2022-06-19 13:51:57 +0530

    yes

--- mern/src/App.js
+++ mern/src/App.js
@@ -1,10 +1,30 @@
+import Data from './data';
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <a href="#">shoopingapp</a>
       </header>
-      <main>list</main>
+      <main>
+        <h1 className="heding">Feature Items</h1>
+        <div className="prodects">
+          {Data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={product.slug}>
+                <img src={product.img} alt={product.name} />
+              </a>
+              <div className="pro-info">
+                <a href={product.slug}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.price}</p>
+                <button type="button">Buy</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
---
+++ mern/src/data.js
@@ -0,0 +1,38 @@
+const Data = {
+  product: [
+    {
+      name: ' Slim-shirt',
+      slug: 'slim-shirt',
+      catagary: 'Shirt',
+      img: 'images/3.jpg',
+      price: 3000,
+      barnd: 'Nike',
+      rating: 4.6,
+      numreviews: 10,
+      Discription: 'high Qulity',
+    },
+    {
+      name: 'Adidas Slim-shirt',
+      slug: 'Adidass lim-shirt',
+      catagary: 'Shirt',
+      img: '/images/3.jpg',
+      price: 3000,
+      barnd: 'Adidas',
+      rating: 4.6,
+      numreviews: 10,
+      Discription: 'high Qulity',
+    },
+    {
+      name: ' Slim-pants',
+      slug: 'slim-pants',
+      catagary: 'pants',
+      img: '/images/1.jpg',
+      price: 3000,
+      barnd: 'Nike',
+      rating: 4.6,
+      numreviews: 10,
+      Discription: 'high Qulity',
+    },
+  ],
+};
+export default Data;
